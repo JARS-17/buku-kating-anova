eae19f76061156027ef3f708a97b8c877efb6381
kemasverii <kemasverii@gmail.com>
2024-08-03 15:50:03 +0700

    kk

--- Halaman Utama/halaman_utama.py
+++ Halaman Utama/halaman_utama.py
@@ -203,6 +203,13 @@ elif menu == "About Us":
                 "fun_fact": "masak telor setengah sapi",
                 "motto_hidup": "drakor anytime"
             },
+            {
+                "nama": "kk",
+                "sebagai":"Anggota",
+                "nim": "122450007",
+                "fun_fact": "masak telor setengah sapi",
+                "motto_hidup": "drakor anytim"
+            },
         ]
         display_images_with_data(gambar_urls, data_list)
 
