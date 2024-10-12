ff3fba721b86e485eaae08bc47cd267e447d2a95
nurulizzahistiqomah <izzahistiqomah08@gmail.com>
2024-10-13 01:13:41 +0700

    Update foto sekjen juesi

--- Buku Kating/085_Juesi Apridelia Saragih.py
+++ Buku Kating/085_Juesi Apridelia Saragih.py
@@ -97,7 +97,7 @@ menu = streamlit_menu()
 if menu == "Kesekjenan":
     def kesekjenan():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id1QxHPQMAXMZh5IPPcRbDXOfCnSn-OMj1V",
+            "https://drive.google.com/uc?export=view&id=1QxHPQMAXMZh5IPPcRbDXOfCnSn-OMj1V",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
@@ -135,7 +135,7 @@ if menu == "Kesekjenan":
                 "alamat": "Jl.Pagar Alam",
                 "hobbi": "Nonton Drakor",
                 "sosmed": "@wulandarimeliza",
-                "kesan": "keren banget kak",  
+                "kesan": "keren banget ka",  
                 "pesan":"semangat ngaspraknya kak !!!"# 1
             },
         
